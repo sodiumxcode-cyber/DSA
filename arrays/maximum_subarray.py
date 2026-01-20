4be9de31617e43e648fd026cce9d8962dffd67f7
Madhav <your_email@example.com>
2026-01-21 00:39:08 +0530

    Add maximum_subbaray.py in arrays

---
+++ arrays/maximum_subarray.py
@@ -0,0 +1,12 @@
+#Leetcode 53 Maximum Subarray
+#My approach
+
+def maxSubArray(self, nums):
+    curr_sum = 0
+    max_sum = float('-inf')
+    for i in nums:
+        curr_sum += i
+        max_sum = max(curr_sum, max_sum)
+        if curr_sum < 0:
+            curr_sum = 0
+    return max_sum
