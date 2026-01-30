26ba6c9758b734d28b97ff503c4e91a72cc6e956
Madhav <your_email@example.com>
2026-01-30 19:22:24 +0530

    Add Leetcode-125

---
+++ arrays/valid_palindrome.py
@@ -0,0 +1,20 @@
+#Leetcode-125 Valid Palindrome
+
+def IsPalindrome(s):
+    left, right = 0, len(s) - 1
+    while left < right:
+
+        while left < right and not s[left].isalnum():
+            left += 1
+        while left < right and not s[right].isalnum():
+            right -= 1
+
+        if s[left].lower() != s[right].lower():
+            return False
+        left += 1
+        right -= 1
+    
+    return True
+
+hehe = IsPalindrome("My name is, Madhav:")
+print(hehe)
