99853c50c2a4a9e62f966134430a675a46cf9549
Madhav <your_email@example.com>
2026-03-10 02:21:47 +0530

    Add increasing_triplet_sebsequence

---
+++ arrays/increasing_triplet_subsequence.py
@@ -0,0 +1,12 @@
+def increasingTriplet(nums):
+    first = float('inf')
+    second = float('inf')
+
+    for num in nums:
+        if num <= first:
+            first = num
+        elif num <= second:
+            second = num
+        else:
+            return True
+    return False
