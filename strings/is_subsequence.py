8c434146451871d1c5ca65bc68f63af3876c199e
Madhav <your_email@example.com>
2026-03-10 18:55:06 +0530

    Add is Subsequence

---
+++ strings/is_subsequence.py
@@ -0,0 +1,8 @@
+#Leetcode-392 Is Subsequence
+def isSubsequence(s, t):
+    i, j = 0, 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+        j += 1
+    return i == len(s)
